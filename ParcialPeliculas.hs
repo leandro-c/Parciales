140c0564873f7a31384cc72394210442139290a2
leandro-c <leandroccasarin@gmail.com>
2016-11-26 00:52:03 -0300

    peliculasDe

--- ParcialPeliculas.hs
+++ ParcialPeliculas.hs
@@ -1,22 +1,40 @@
-crearCatalogo::Pelicula->Catalago->Catalago
+module(crearCatalogo,agregarPelicula,generosCatalago,peliculaDe,mejoresN)where
+
+import Set
+import Map 
+import Heap
+import Pelicula
+
+data Catalago = MKC(Map Genero [Pelicula])(Heap Pelicula)
+
+crearCatalogo::Catalago --0(n)
+crearCatalogo = MKC(emptyM)(emptyH)
 
 agregarPelicula::Pelicula->Catalago->Catalago
+agregarPelicula p MKC(m h) = MKC(adosarPelicula m p)(insertH h p)
+
+adosarPelicula::Map Genero [Pelicula]->Pelicula->Map Genero [Pelicula]
+adosarPelicula  m p = assocM p m
 
 generosCatalago::Catalago->[Genero]
+generosCatalago MKC(m _) = setToList(domM m) 
 
+peliculaDe::Genero->Catalago->[Pelicula]
+peliculaDe g MKC(m _) = peliculaDeGenero m
 
-peliculaDe::Genero->[Pelicula]
-
+peliculaDeGenero::Map Genero [Pelicula]->[Pelicula]
+peliculaDeGenero (Map g []) = []
+peliculaDeGenero m@(Map g ps) = fromJust(lookUp g m)
 mejoresN::Int->Catalago->[Pelicula]
 
-/*Usuario*/
+--/*Usuario*/
 
-cantidadesDe::Catalago->[(Genero,Int)]
+--cantidadesDe::Catalago->[(Genero,Int)]
 
-ordenarPorPuntaje::Genero->Catalago->Heap Pelicula
+--ordenarPorPuntaje::Genero->Catalago->Heap Pelicula
 
-maxGeneroConPeliculas10::Catalago->Genero
+--maxGeneroConPeliculas10::Catalago->Genero
 
-peliculasQueCominezanCon::String->Catalago->Heap Pelicula
+--peliculasQueCominezanCon::String->Catalago->Heap Pelicula
 
 
